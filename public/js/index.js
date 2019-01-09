c4347affce20f4fab27b6337f07c8eb1a52e7142
uttam_mali <uttam@localhost.localdomain>
2019-01-09 17:22:01 +0530

    auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 
 var socket = io();
+
+function scrollToBottom()
+{
+    var messages = $("#messages");
+    var newMessage = messages.children("li:last-child");
+    
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop("scrollTop");
+    var scrollHeight = messages.prop("scrollHeight");
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+    {
+        messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on("connect", function() 
 {
     console.log("connected to server..");
@@ -23,6 +41,7 @@ socket.on("newMessage", function (message)
         createdAt: formatedTime
     });
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 socket.on("newLocationMessage", function (message) 
@@ -37,13 +56,9 @@ socket.on("newLocationMessage", function (message)
         from: message.from,
         createdAt: formatedTime
     });
-    
-    // var li = $("<li></li>");
-    // var a = $("<a target = '_blank'>My current location</a>");
-    // li.text(`${message.from} ${formatedTime}: `);
-    // a.attr("href", message.url);
-    // li.append(a);
+ 
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 $("#form").on("submit", function (e) 
