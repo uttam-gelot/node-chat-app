d73dddd3629a4d06f57c50044032da575d8bef5c
uttam_mali <uttam@localhost.localdomain>
2019-01-09 16:56:53 +0530

    message design

--- public/js/index.js
+++ public/js/index.js
@@ -15,21 +15,35 @@ socket.on("disconnect", function()
 socket.on("newMessage", function (message) 
 {
     var formatedTime = moment().format("h:mm a");
-    var li = $("<li></li>");
-    li.text(`${message.from} ${formatedTime}: ${message.text}`);
-    $("#messages").append(li);
+    var template = $("#message-template").html();
+    var html = Mustache.render(template,
+    {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    $("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function (message) 
 {
     
     var formatedTime = moment().format("h:mm a");
-    var li = $("<li></li>");
-    var a = $("<a target = '_blank'>My current location</a>");
-    li.text(`${message.from} ${formatedTime}: `);
-    a.attr("href", message.url);
-    li.append(a);
-    $("#messages").append(li);
+
+    var template = $("#location-message-template").html();
+    var html = Mustache.render(template,
+    {
+        url: message.url,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    
+    // var li = $("<li></li>");
+    // var a = $("<a target = '_blank'>My current location</a>");
+    // li.text(`${message.from} ${formatedTime}: `);
+    // a.attr("href", message.url);
+    // li.append(a);
+    $("#messages").append(html);
 });
 
 $("#form").on("submit", function (e) 
